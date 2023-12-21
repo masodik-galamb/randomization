b82f9c2c322a5b4d502169f97b5149720c8b82c3
Max <masodik.galamb@gmail.com>
2023-12-21 01:02:14 +0100

    1st time win v1

--- luckyNumber/main.go
+++ luckyNumber/main.go
@@ -25,14 +25,20 @@ import (
 //  3. Terminate the game and print the bonus message
 // ---------------------------------------------------------
 
-func main() {
+const (
+	usage = `🍀                                                   🍀
+	I want to play a game with you.
+Pick up some number and let's see how lucky you are...
+🍀                                                   🍀`
+	positive   = `Provide a positive, non-zero number.`
+	maxTurns   = 3
+	regularWin = `🎉 You won! Random nubber was`
+	superWin   = `🥇 Wow! You won from the first turn! Seems like you're super lucky!
+Random nubber was indeed`
+	loose = `Sorry, you're not a winner...`
+)
 
-	const (
-		usage = `I want to play a game with you.
-Pick up some number and let's see how lucky you are.`
-		positive = `Provide a positive, non-zero number.`
-		maxTurns = 3
-	)
+func main() {
 
 	//rand.Seed(time.Now().UnixNano())
 	rand.NewSource(time.Now().UnixNano())
@@ -49,25 +55,23 @@ Pick up some number and let's see how lucky you are.`
 		fmt.Printf("It's is not a number.")
 		return
 	}
+
 	if guess <= 0 {
 		fmt.Println(positive)
 		return
 	}
 
-	for turn := 0; turn < maxTurns; turn++ {
+	for turn := 1; turn < maxTurns; turn++ {
 		n := rand.Intn(guess + 1)
 
-		fmt.Println(turn)
-
 		if n == guess {
-			fmt.Println("You win!")
-			fmt.Printf("Random nubber was %d\n", n)
-			if turn == 0 {
-				fmt.Println("Wow! You win from the first move! Seems like you're super lucky!")
+			if turn == 1 {
+				fmt.Println(superWin, n)
+			} else {
+				fmt.Println(regularWin, n)
 			}
 			return
 		}
 	}
-	fmt.Println("You loose! Wanna try again?")
-
+	fmt.Println(loose)
 }
